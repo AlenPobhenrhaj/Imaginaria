ef710a6ec4f9e716bd989340d7b27044b7f29480
AlenPobhenrhaj <alenprem4@gmail.com>
2023-06-26 00:32:03 +0800

    Added test video files in Screen1

--- app/src/main/java/com/example/imaginaria/app/Screen1Activity.kt
+++ app/src/main/java/com/example/imaginaria/app/Screen1Activity.kt
@@ -1,12 +1,32 @@
 package com.example.imaginaria.app
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
 import com.example.imaginaria.R
+import com.example.imaginaria.databinding.ActivityScreen1Binding
 
 class Screen1Activity : AppCompatActivity() {
+    private lateinit var binding: ActivityScreen1Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_screen1)
+        binding = ActivityScreen1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Create a MediaController and set it to your VideoView
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(binding.simpleVideoView)
+
+        // Set the MediaController for the VideoView
+        binding.simpleVideoView.setMediaController(mediaController)
+
+        // Set the Uri for the VideoView
+        val videoUri = Uri.parse("android.resource://" + packageName + "/" + R.raw.test)
+        binding.simpleVideoView.setVideoURI(videoUri)
+
+        // Start the VideoView
+        binding.simpleVideoView.start()
     }
 }
